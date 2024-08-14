8c4a76406bd335557b68efc67b5a5d6e0b904ffa
util.js <6007473+cron-tab@users.noreply.github.com>
2024-08-14 16:36:34 +0900

    feat: daily-ui

--- design-system/apps/docs/.storybook/main.js
+++ design-system/apps/docs/.storybook/main.js
@@ -25,8 +25,8 @@ const config = {
       resolve: {
         alias: [
           {
-            find: "ui",
-            replacement: resolve(__dirname, "../../../packages/ui/"),
+            find: "daily-ui",
+            replacement: resolve(__dirname, "../../../packages/daily-ui/"),
           },
         ],
       },
