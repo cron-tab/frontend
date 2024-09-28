1b309ee46c1f9caa015ee5436016035d146d6746
util.js <6007473+cron-tab@users.noreply.github.com>
2024-09-28 17:04:33 +0900

    [app-expo] edit: webview uri for ios, aos

--- services/app-expo/app/(tabs)/index.tsx
+++ services/app-expo/app/(tabs)/index.tsx
@@ -1,10 +1,12 @@
-import {StyleSheet} from 'react-native';
+import {Platform, StyleSheet} from 'react-native';
 import {WebView} from 'react-native-webview';
 
 export default function HomeScreen() {
+    const uri = Platform.OS === 'ios' ? 'http://localhost:3000/' : 'http://10.0.2.2:3000/';
+
     return (
         <WebView
-            source={{uri: 'http://localhost:3000/'}}
+            source={{uri}}
             style={styles.webview}
         />
     );
